3f4508336436c8d253bc8a40c998932937733abc
IvanovKS <Skitchi@mail.ru>
2024-04-11 20:32:46 +0300

    feat: make tasks 2

--- src/homeWork_02.js
+++ src/homeWork_02.js
@@ -34,6 +34,50 @@ class Count_ {
 }
 const counter6 = new Count_('six');
 
+// * Задание 2
+const simpleCounter = {
+  count: 'one',
+};
+
+const deepCounter = {
+  count: 'one',
+  deep: {
+    one: 1,
+    two: {
+      a: 1,
+      b: 2
+    }
+  }
+};
+
+// 1-й способ (поверхностное копирование):
+const copiedCounter1 = {...simpleCounter};
+
+// 2-й способ (поверхностное копирование):
+const copiedCounter2 = Object.assign({}, simpleCounter);
+
+// 3-й способ (глубокое копирование):
+const copiedCounter3 = structuredClone(deepCounter);
+
+// 4-й способ (глубокое копирование):
+const copiedCounter4 = JSON.parse(JSON.stringify(deepCounter));
+
+// 5-й способ (глубокое копирование):
+function getDeepCopy(argument) {
+  if (Array.isArray(argument)) {
+    return argument.map(item => getDeepCopy(item));
+  } else if (typeof argument === 'object' && argument !== null) {
+    const obj = {};
+    for (let key in argument) {
+      obj[key] = getDeepCopy(argument[key]);
+    }
+    return obj;
+  } else {
+    return argument;
+  }
+}
+const copiedCounter5 = getDeepCopy(deepCounter);
+
 // * Задание 3
 // 1-й способ (function declaration):
 function makeCounter1(argument) {
@@ -88,6 +132,27 @@ console.log(makeCounter6);
 console.log(makeCounter7());
 console.log(makeCounter8.showCount());
 console.groupEnd();
+
+// * Задание 4
+// Глобальный structuredClone() метод создает глубокий клон и обеспечивает структурированное клонирование объекта, включая все его вложенные объекты, массивы, мапы, сеты и т.п. Этот метод также позволяет переносить переносимые объекты в исходном значении, а не клонировать их в новый объект. Перенесенные объекты отделяются от исходного объекта и прикрепляются к новому объекту; они больше не доступны в исходном объекте.
+// Синтаксис:
+// structuredClone(value)
+// structuredClone(value, options), где
+// value - Объект, который необходимо клонировать. Это может быть любой структурированный клонируемый тип;
+// options (опционально) - Объект со следующими свойствами (transfer): Массив передаваемых объектов , которые будут перемещены, а не клонированы в возвращаемый объект. Перенос делает исходный объект непригодным для использования
+
+// Пример клонирования объекта:
+// const mushrooms1 = {
+//   amanita: ["muscaria", "virosa"],
+// };
+// const mushrooms2 = structuredClone(mushrooms1);
+
+// Пример переноса объекта:
+// const buffer1 = new ArrayBuffer(16);
+// const object1 = {
+//   buffer: buffer1,
+// };
+// const object2 = structuredClone(object1, { transfer: [buffer1] });
 
 // * Бонус задание 2
 
